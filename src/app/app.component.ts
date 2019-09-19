bef699137ec97a0ef1b38003715c534265652110
Unknown <richard.m@inreality.com>
2019-09-19 21:31:46 +0530

    Default and user defined settings added and handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Ng2CarouselItem } from './models/ng2-carousel-item.model';
+import { Ng2CarouselComponent } from './ng2-carousel/ng2-carousel.component';
+import { Ng2CarouselSetting } from './models/ng2-carousel-setting.model';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +9,33 @@ import { Ng2CarouselItem } from './models/ng2-carousel-item.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng2-carousel';
 
   items: Array<Ng2CarouselItem> = []
+
+  setting: Ng2CarouselSetting;
+
+  @ViewChild('c1', {static: true}) c1: Ng2CarouselComponent;
 
   constructor() {
     this.items = []
     this.items.push({src: 'assets/4k-wallpaper-afterglow-beach-2834758.jpg'})
     this.items.push({src: 'assets/4k-wallpaper-astronomy-astrophotography-2666598.jpg'})
     this.items.push({src: 'assets/animal-animal-photography-blur-145939.jpg'})
-    this.items.push({src: 'assets/4k-wallpaper-afterglow-beach-2834758.jpg'})
-    this.items.push({src: 'assets/4k-wallpaper-astronomy-astrophotography-2666598.jpg'})
-    this.items.push({src: 'assets/animal-animal-photography-blur-145939.jpg'})
-    this.items.push({src: 'assets/4k-wallpaper-afterglow-beach-2834758.jpg'})
-    this.items.push({src: 'assets/4k-wallpaper-astronomy-astrophotography-2666598.jpg'})
-    this.items.push({src: 'assets/animal-animal-photography-blur-145939.jpg'})
+
+    this.setting = {
+      autoPlay: true,
+      showArrow: false,
+      holdTime: 5000,
+      showDots: true
+    }
+  }
+
+  playNext() {
+    this.c1.playNext()
+  }
+
+  playPrev() {
+    this.c1.playPrev()
   }
 
 }
---
+++ src/app/models/ng2-carousel-setting-default.model.ts
@@ -0,0 +1,8 @@
+import { Ng2CarouselSetting } from './ng2-carousel-setting.model'
+
+export const DEFAULT: Ng2CarouselSetting =  {
+  autoPlay: false,
+  showArrow: true,
+  holdTime: 15000,
+  showDots: true
+}
