b0c78472c286a143dbd39b48a6434f27d6c6be67
Unknown <richard.m@inreality.com>
2019-09-21 22:15:08 +0530

    Set auto play as true in default setting

--- src/app/models/ng2-carousel-setting-default.model.ts
+++ src/app/models/ng2-carousel-setting-default.model.ts
@@ -2,7 +2,7 @@ import { Ng2CarouselSetting } from './ng2-carousel-setting.model'
 import { Ng2AnimationType } from './ng2-carousel-animation-type.model'
 
 export const DEFAULT: Ng2CarouselSetting =  {
-  autoPlay: false,
+  autoPlay: true,
   showArrow: true,
   holdTime: 15000,
   showDots: true,
